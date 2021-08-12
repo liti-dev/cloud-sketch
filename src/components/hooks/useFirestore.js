9bfb8d9c769ada57cc11245e20c58dbb19eb5020
Thuyet <akita311@gmail.com>
2021-08-12 08:20:22 +0700

    update imagelist

---
+++ src/components/hooks/useFirestore.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { projectFirestore } from '../../firebase/config';
+const useFirestore = (collection) => {
+  const [docs, setDocs] = useState([]);
+  useEffect(() => {
+    const unsub = projectFirestore
+      .collection(collection)
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snap) => {
+        let documents = [];
+        snap.forEach((doc) => {
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+        setDocs(documents);
+      });
+    // Cleanup function
+    return () => unsub();
+  }, [collection]);
+  return { docs };
+};
+export default useFirestore;
